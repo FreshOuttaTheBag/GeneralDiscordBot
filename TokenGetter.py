c48adb2727f27f00039fbd23761c77b1e6e81b51
Corey <coreycoleson@gmail.com>
2020-06-17 00:13:54 -0400

    Even more token getter

--- TokenGetter.py
+++ TokenGetter.py
@@ -1,7 +1,8 @@
 ''' This file is in the place of a config file for now :P
-    Make the TOKEN variable your api token. '''
+    Make the TOKEN variable your api token.
+    DO NOT STAGE AND PUSH WHEN THE TOKEN VARIABLE IS SET'''
 
 
 def getToken():
     TOKEN = ''
-    return token
+    return TOKEN
